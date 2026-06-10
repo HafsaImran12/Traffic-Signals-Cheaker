2fe7921c1cafbccce5f0ea3ecf42e090724590be
Hafsa Imran <hafsaemran1712@gmail.com>
2026-06-10 14:24:36 +0500

    Traffic-Signal-Cheaker

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-let btnRed = document.querySelector(".btnRed");
-let btnYellow = document.querySelector(".btnYellow");
-let btnGreen = document.querySelector(".btnGreen");
+let result = document.querySelector(".result")
+let red = document.querySelector("#red")
+let yellow = document.querySelector("#yellow")
+let green = document.querySelector("#green")
 
-btnRed.addEventListener("click", () => {
-    document.body.className= "red"
-});
-
-btnYellow.addEventListener("click", () => {
-    document.body.className= "yellow"
-});
-
-btnGreen.addEventListener("click", () => {
-    document.body.className= "green"
-});
+red.addEventListener("click", () => {
+    result.innerHTML = "Stop"
+    redColor.classList.add ("redLight")
+    yellowColor.classList.remove ("yellowLight")
+    greenColor.classList.remove ("greenLight")
+    
+})
+yellow.addEventListener("click", () => {
+    result.innerHTML = "Start"
+    redColor.classList.remove ("redLight")
+    yellowColor.classList.add ("yellowLight")
+    greenColor.classList.remove ("greenLight")
+})
+green.addEventListener("click", () => {
+    result.innerHTML = "Go"
+    redColor.classList.remove ("redLight")
+    yellowColor.classList.remove ("yellowLight")
+    greenColor.classList.add ("greenLight")
+})
